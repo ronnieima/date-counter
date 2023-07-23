36a48c6b37cc7efe59556a48fe870cebbb2b455d
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-07-23 17:45:17 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  function handlePreviousStep() {
+    setStep((step) => step - 1);
+  }
+
+  function handleNextStep() {
+    setStep((step) => step + 1);
+  }
+
+  function handlePreviousCount() {
+    setCount((count) => count - step);
+  }
+
+  function handleNextCount() {
+    setCount((count) => count + step);
+  }
+
+  return (
+    <div className="counter">
+      <TextWithCounters
+        text="Step"
+        counter={step}
+        handlers={[handlePreviousStep, handleNextStep]}
+      />
+      <TextWithCounters
+        text="Count"
+        counter={count}
+        handlers={[handlePreviousCount, handleNextCount]}
+      />
+      <DateCounter count={count} />
+    </div>
+  );
+}
+
+function DateCounter({ count }) {
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+  return (
+    <p>
+      {count < 0 && `${count} days ago was ${date.toDateString()}`}
+      {count === 0 && `Today is ${date.toDateString()}`}
+      {count > 0 && `${count} days from today is ${date.toDateString()}`}
+    </p>
+  );
+}
+
+function TextWithCounters({ text, counter, handlers }) {
+  return (
+    <div className="buttonText">
+      <button onClick={handlers[0]}>-</button>
+      <span>{`${text}: ${counter}`}</span>
+      <button onClick={handlers[1]}>+</button>
+    </div>
+  );
+}
